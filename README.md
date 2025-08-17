ea2d137d0e4d01e79dc60a1112e73bb0f57cea75
valeryjohannasolartemogollon <valeryjohannasolartemogollon@gmail.com>
2025-08-17 18:11:16 -0400

    proyecto terminado

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# challenge-amigo-secreto_esp-main
+
+# Aplicación Amigo Secreto
+
+Esta aplicación permite a los usuarios ingresar nombres de amigos en una lista y realizar un sorteo aleatorio para determinar quién es el "amigo secreto".
+
+## Funcionalidades
+
+- Agregar nombres mediante un campo de texto y el botón **Adicionar**.
+- Visualizar la lista de nombres ingresados en la página.
+- Al finalizar, presionar el botón **Sortear Amigo** para seleccionar aleatoriamente uno de los nombres y mostrar el resultado en pantalla.
+
+## Uso
+
+1. Ingresa los nombres de los participantes en el campo de texto y haz clic en **Adicionar**.
+2. Repite el proceso hasta agregar todos los nombres.
+3. Haz clic en **Sortear Amigo** para realizar el sorteo y ver quién es el amigo secreto.
+
+---
+¡Diviértete descubriendo quién será el amigo secreto!
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.toUpperCase();
+    if (amigo === "") {
+        alert("Por favor ingresa un nombre.");
+        return;
+    }
+    nombres.push(amigo);
+    document.getElementById("amigo").value = "";
+    lista(nombres);
+
+}
+
+function lista(nombres) {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; // Limpiar la lista antes de agregar nuevos amigos
+    for (let i = 0; i < nombres.length; i++) {
+        listaAmigos.innerHTML += `<li>${nombres[i]}</li>`;
+
+    }
+}
+
+function sortearAmigo() {
+    if (nombres.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    let amigoSorteado = nombres[Math.floor(Math.random() * nombres.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+}
+
